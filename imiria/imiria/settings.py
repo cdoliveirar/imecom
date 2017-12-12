c18ac39b2a7c7a9dfd2134d37369ddc08f3fd052
Carlos Oliveira <carlos@cerouno.carlos.net>
2017-12-12 12:12:56 -0500

    modify setting dev

--- imiria/imiria/settings.py
+++ imiria/imiria/settings.py
@@ -16,6 +16,7 @@ from oscar import OSCAR_MAIN_TEMPLATE_DIR
 from oscar import get_core_apps
 
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
